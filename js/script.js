67e237b0dda7dc62c7c436ba2ce11e61491c652a
Fwzryy <raihan0603@gmail.com>
2024-08-14 11:28:52 +0700

    Add feature js

--- js/script.js
+++ js/script.js
@@ -29,4 +29,63 @@ function calculate(){
     countBmi();
   }
 
+} 
+  
+// ---- Membuat fungsi countBmi() ----
+  function countBmi() {
+    var p = [age.value, height.value, weight.value];
+    // mendeklarasikan variabel p dan menginisialisasinya dengan sebuah array [] yang berisi 3 nilai dari tiga element: "age","height","weight" (Nilai ini diambil dari input user)
+    if(male.checked) { // kondisi yang memeriksa apakah pilihan gender "male" atau "female" telah dipilih dan menambahkan string ke array p.
+      p.push("male");
+    } else if (female.checked) {
+      p.push("female");
+
+      // example: jika memilih male, array p berisi nilai ["age","height","weight","male"]
+    }
+  
+
+  // ---- Menghitung Indeks Massa Tubuh(BMI) ----
+  var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+  /**
+   * Number(p[2]): 
+   * mengonversi nilai dari elemen ketiga dalam array p (weight) menjadi tipe data angka (number).
+   * 
+   * Number(p[1])/100: 
+   * mengonversi nilai dari elemen kedua dalam array p (height) menjadi tipe data angka dan membaginya dengan 100. Ini mengubah tinggi dari sentimeter menjadi meter.
+   * 
+   * Number(p[1])/100*Number(p[1])/100:
+   *  Ini mengalikan tinggi yang sudah dikonversi menjadi meter dengan dirinya sendiri (untuk menghitung kuadrat tinggi dalam meter).
+   * 
+   * Number(p[2])/(Number(p[1])/100*Number(p[1])/100): Ini membagi berat badan dengan kuadrat tinggi dalam meter untuk menghitung BMI.
+   */
+
+  var result = ''; //nilai default var result kosong
+  if(bmi<18.5){
+    result = 'Underweight'; //jika nilai BMI kurang dari 18.5, result = 'Underweight'
+     }else if(18.5<=bmi&&bmi<=24.9){ //jika nilai BMI diantara 18.5 sampai 24.9, result = 'Healthy'
+    result = 'Healthy';
+     }else if(25<=bmi&&bmi<=29.9){ //jika nilai BMI diantara 25 sampai 29.9, result = 'Overweight'
+    result = 'Overweight';
+     }else if(30<=bmi&&bmi<=34.9){//jika nilai BMI diantara 30 sampai 34.9, result = 'Obese'
+    result = 'Obese';
+     }else if(35<=bmi){//jika nilai BMI lebih atau sama dengan 35, result = 'Extremely Obese'
+    result = 'Extremely obese';
+  }
+
+  resultArea.style.display = "block"; //mengatur style element resultArea menjadi block
+  document.querySelector(".comment").innerHTML = `You are <span id="comment">${result}</span>`;//mengubah isi HTML dengan string "You are" {result(menampilkan isi result)} 
+  document.querySelector("#result").innerHTML = bmi.toFixed(2);//mengatur isi HTML-nya menjadi nilai BMI (dibulatkan hingga dua desimal) yang ada dalam variabel bmi.
+}
+
+
+// Ketika pengguna mengklik <span> (x), tutup modal. 
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Saat pengguna mengklik di mana saja di luar modal, tutup modal tersebut
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
